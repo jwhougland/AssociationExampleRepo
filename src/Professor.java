af20518a0c6cda1c3856f5b7c95d793b1ef94d67
jwhougland <47694635+jwhougland@users.noreply.github.com>
2023-11-20 14:49:22 -0600

    Updating Professor class to allow associations with Student instances

--- src/Professor.java
+++ src/Professor.java
@@ -1,4 +1,6 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Defines the characteristics of a professor.
@@ -11,13 +13,25 @@ public class Professor extends UniversityMember {
     private List<Enums.Discipline> disciplines;
 
     /**
+     * Defines the professor's job role
+     */
+    private Enums.ProfessorJobRole jobRole;
+
+    /**
+     * Map where the key is a student's university ID and the
+     * value is the student that is associated with this professor.
+     */
+    private Map<Integer, Student> students = new HashMap<>();
+
+    /**
      * Creates a fully initialized professor using the given data.
      *
      * @param name         Name of the professor
      * @param id           ID of the professor
      * @param disciplines  Collection of disciplines that the professor teaches and/or researches.
+     * @param jobRole      Defines the professor's job role
      */
-    public Professor(String name, int id, List<Enums.Discipline> disciplines) {
+    public Professor(String name, int id, List<Enums.Discipline> disciplines, Enums.ProfessorJobRole jobRole) {
         super(name, id);
 
         // Run a null check on the disciplines collection
@@ -26,6 +40,7 @@ public class Professor extends UniversityMember {
         }
 
         this.disciplines = disciplines;
+        this.jobRole = jobRole;
     }
 
     /**
@@ -33,5 +48,44 @@ public class Professor extends UniversityMember {
      */
     public List<Enums.Discipline> getDisciplines() {
         return disciplines;
+    }
+
+    /**
+     * Returns the professor's job role.
+     */
+    public Enums.ProfessorJobRole getJobRole() {
+        return jobRole;
+    }
+
+    /**
+     * Adds the student to the list that are associated with the professor.
+     *
+     * @param student Student to add to the list that are associated with the professor.
+     */
+    public void addStudentAssociation(Student student) {
+
+        // Run a null check on the student parameter
+        if (student == null) {
+            throw new IllegalArgumentException("Cannot associate a null student with a professor");
+        }
+
+        // Add the student to the map
+        students.putIfAbsent(student.getId(), student);
+    }
+
+    /**
+     * Removes the student from the list that are associated with the professor.
+     *
+     * @param student Student to remove from the list that are associated with the professor.
+     */
+    public void removeStudentAssociation(Student student) {
+
+        // Run a null check on the student parameter
+        if (student == null) {
+            throw new IllegalArgumentException("Cannot disassociate a null student from a professor");
+        }
+
+        // Remove the student from the map
+        students.remove(student.getId());
     }
 }
