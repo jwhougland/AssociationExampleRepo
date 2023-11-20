17f1e3f2f18142638cbcced21e6dfe44813cade3
jwhougland <47694635+jwhougland@users.noreply.github.com>
2023-11-20 10:46:52 -0600

    Initial source code pushed to confirm connection with remote origin

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Main class for the association example console app.
+ */
+public class Main {
+    /**
+     * Entry point for the association example console app.
+     * @param args Not used.
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
